bae5de003ce4f42b9cddbb681798fba482472449
Saifmk10 <saifmkpvt@gmail.com>
2025-08-06 17:05:22 +0530

    [TEST MODEL] - Tested a model that takes user input as voice and converts it into toekns --> 768 numeric format

--- test_models/initialModel.py
+++ test_models/initialModel.py
@@ -1,12 +1,29 @@
+# this is a code that is used to conver the raw text a user enters into a token format and eventually into 
+
 from transformers import DistilBertTokenizer , DistilBertModel;
 import torch;
+import speech_recognition as sr
+
+audioInput = sr.Recognizer()
+with sr.Microphone() as source :
+    print("Speak something im listening: ")
+    audio = audioInput.listen(source)
+    
+    try:
+        userInputViaVoiceModule = audioInput.recognize_google(audio)
+        print(f"User Said: {userInputViaVoiceModule}")
+    except sr.UnknownValueError:
+        print("Sorry, I could not understand the audio.")
+    except sr.RequestError:
+        print("Could not request results, check your internet connection.")
+
 
 tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 model = DistilBertModel.from_pretrained("distilbert-base-uncased")
 
-text = input("Enter anything you like : ")
+textViaVoice = userInputViaVoiceModule
 
-userinput = tokenizer(text , return_tensors = "pt")
+userinput = tokenizer(textViaVoice , return_tensors = "pt")
 
 print(f"TOKENIZED DATA : {userinput}")
 
@@ -15,4 +32,5 @@ with torch.no_grad() :
     finalOutput = output.last_hidden_state
     print(finalOutput)
 
-    
+# realinput = tokenizer.decode(userinput)
+# print(realinput)
